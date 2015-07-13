394857396282719c2aaa91dc61e5271f03619acb
mathieucarbou <mathieu.carbou@gmail.com>
2015-07-13 08:57:44 -0400

    app direct oauth

--- src/main/groovy/me/carbou/mathieu/tictactoe/rest/AppDirectResource.groovy
+++ src/main/groovy/me/carbou/mathieu/tictactoe/rest/AppDirectResource.groovy
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2015 Mathieu Carbou (mathieu@carbou.me)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -37,6 +37,8 @@ import javax.ws.rs.core.Response
 @Path("/api/appdirect")
 class AppDirectResource {
 
+    String wwwAuthenticateHeader = "OAuth realm=\"Tic-Tac-Toe\""
+
     @Inject Client client
     @Inject OAuth1Signature oAuthSignature
 
@@ -59,7 +61,7 @@ OpenID Realm: Realm for the OpenID consumer. Setting this parameter correctly en
 
     @GET
     @Path("subscription/{event}/{token}")
-    @Produces("application/xml; charset=utf-8")
+    @Produces("application/xml charset=utf-8")
     String subscriptionCreated(@PathParam("event") String event,
                                @PathParam("token") String token,
                                @QueryParam("src") String eventUrl,
@@ -71,8 +73,8 @@ OpenID Realm: Realm for the OpenID consumer. Setting this parameter correctly en
 
         println("\nRequest: ${request.uriInfo.requestUri}\nQuery: ${request.uriInfo.queryParameters}\nHeaders: ${request.headers}")
 
-        // commented since AppDirect OAuth signature does not match RFC standard for OAuth 1.0 and the signature validation process is not documented at http://info.appdirect.com/developers/docs/api_integration/oauth_api_authentication
-        verifySignature(request)
+        //should be moved
+        verifyOAuthSignature(request)
 
         String xml = client.target("https://www.appdirect.com/api/integration/v1/events/${token}")
             .request(MediaType.APPLICATION_XML_TYPE)
@@ -102,15 +104,18 @@ OpenID Realm: Realm for the OpenID consumer. Setting this parameter correctly en
 </result>"""
     }
 
-    private void verifySignature(ContainerRequestContext request) throws OAuth1SignatureException {
-        String wwwAuthenticateHeader = "OAuth realm=\"Tic-Tac-Toe\""
-        String auth = request.getHeaderString(HttpHeaders.AUTHORIZATION);
-        if (auth == null) throw new NotAuthorizedException("Missing OAuth", wwwAuthenticateHeader);
-        OAuthServerRequest osr = new OAuthServerRequest(request);
-        OAuth1Parameters params = new OAuth1Parameters().readRequest(osr);
-        OAuth1Secrets secrets = new OAuth1Secrets().consumerSecret(Env.APPDIRECT_SECRET);
+    private void verifyOAuthSignature(ContainerRequestContext request) throws OAuth1SignatureException {
+        String auth = request.getHeaderString(HttpHeaders.AUTHORIZATION)
+        if (auth == null) throw new NotAuthorizedException("Missing OAuth", wwwAuthenticateHeader)
+
+        OAuthServerRequest osr = new OAuthServerRequest(request)
+        OAuth1Parameters params = new OAuth1Parameters().readRequest(osr)
+
+        if (!params) throw new NotAuthorizedException("Missing OAuth", wwwAuthenticateHeader)
+
+        OAuth1Secrets secrets = new OAuth1Secrets().consumerSecret(Env.APPDIRECT_SECRET)
         if (!oAuthSignature.verify(new OAuthServerRequest(request), params, secrets)) {
-            throw new NotAuthorizedException("Invalid OAuth", wwwAuthenticateHeader);
+            throw new NotAuthorizedException("Invalid OAuth", wwwAuthenticateHeader)
         }
     }
 
